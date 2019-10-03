157e5d051e3de26695aa341da189cf97f9b6986f
JoshuaE <15404@LAPTOP-ABMUOFI9>
2019-10-03 17:46:29 -0400

    try

--- SignalRChat/ChatHub.cs
+++ SignalRChat/ChatHub.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 
+
+
+
 namespace SignalRChat
 
 
@@ -21,9 +24,15 @@ namespace SignalRChat
     //
     //}
 
+    
+       
     public class ChatHub : Hub
     {
-
+        //not best way
+        //https://www.codeproject.com/Questions/233650/How-to-define-Global-veriable-in-Csharp-net
+        static int whoseturn = 0;
+        
+       
         //clients : found in 
         clients A_client = new clients();
 
@@ -59,6 +68,41 @@ namespace SignalRChat
         //{
         //    
         //}
+
+        public void printturn()
+        {
+            string name = "";
+            if (whoseturn == 0)
+            {
+
+                //Clients.Client(ClientList[1].ConnectionId).printturn(name);
+                name = ClientList[1].Name;
+            }
+            //second player
+            if (whoseturn == 1)
+            {
+                //Clients.Client(ClientList[0].ConnectionId).printturn(name);
+                name = ClientList[0].Name;
+            }
+
+            if (whoseturn == 0)
+            {
+                whoseturn = 1;
+            }
+            else if (whoseturn == 1  )
+            {
+                whoseturn = 0;
+            }
+
+            //get rid of whoseturn
+            Clients.Client(ClientList[0].ConnectionId).printname(name);
+            Clients.Client(ClientList[1].ConnectionId).printname(name);
+            //return (name);
+
+        }
+
+            
+
         public void register(string name)
         {
              Debug.Write("play");
@@ -71,16 +115,17 @@ namespace SignalRChat
         //1 or zero
         public void play(string name, string message)
         {
-            Debug.Write("play");
-           // for (int i = 0; i <= 1; i++)
+            //Debug.Write("play");
+            //for (int i = 0; i <= 1; i++)
             //{
-                //if (name == ClientList[i].Name)
-                //{
+               if (name == ClientList[whoseturn].Name)
+                {
                     //if (ClientList[i].ConnectionId == Context.ConnectionId)
                     //{
                         if (message == "1")
                         {
 
+                            //disabled buttons!
                             //Clients.AllExcept().pressbutton("1");
                             Clients.Client(ClientList[0].ConnectionId).pressbutton(message);
                             Clients.Client(ClientList[1].ConnectionId).pressbutton(message);
@@ -90,10 +135,11 @@ namespace SignalRChat
 
                             Clients.Client(ClientList[0].ConnectionId).pressbutton(message);
                             Clients.Client(ClientList[1].ConnectionId).pressbutton(message);
-            }
-                    //}
-                //}
-            //}
+                        }
+                 
+               }
+               //printturn();
+
         }
     }
 
